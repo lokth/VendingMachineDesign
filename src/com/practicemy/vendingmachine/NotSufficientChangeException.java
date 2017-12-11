e9322e0310b354851469141f1c85e5bdeda8cc1a
lokth <thapa.lok19@gmail.com>
2017-12-11 14:29:56 -0500

    exception handeling

---
+++ src/com/practicemy/vendingmachine/NotSufficientChangeException.java
@@ -0,0 +1,16 @@
+package com.practicemy.vendingmachine;
+
+public class NotSufficientChangeException extends RuntimeException {
+
+        private String message;
+
+        public NotSufficientChangeException(String string){
+            this.message = string;
+        }
+
+        @Override
+        public String getMessage(){
+            return message;
+        }
+
+}
---
+++ src/com/practicemy/vendingmachine/SoldOutException.java
@@ -0,0 +1,16 @@
+package com.practicemy.vendingmachine;
+
+public class SoldOutException extends RuntimeException {
+
+    private String message;
+
+    public SoldOutException(String message){
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage(){
+        return message;
+    }
+
+}
