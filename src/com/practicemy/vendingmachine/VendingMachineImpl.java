5f345d791ddc842f42709449472df2fae15d510d
lokth <thapa.lok19@gmail.com>
2017-12-11 21:22:01 -0500

    uml diagram and implementation class

--- src/com/practicemy/vendingmachine/VendingMachineImpl.java
+++ src/com/practicemy/vendingmachine/VendingMachineImpl.java
@@ -1,5 +1,7 @@
 package com.practicemy.vendingmachine;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class VendingMachineImpl implements VendingMachine {
@@ -15,33 +17,149 @@ public class VendingMachineImpl implements VendingMachine {
     }
 
     private void initialize(){
-//        for(Coin c : Coin.values()){
-//
-//        }
+        for(Coin c : Coin.values()){
+            cashInventory.put(c, 5);
+        }
+
+        for(Item i : Item.values()){
+            itemInventory.put(i, 5);
+        }
     }
 
     @Override
     public long selectItemAndGetPrice(Item item) {
-        return 0;
+        if(itemInventory.hasItem(item)) {
+            currentItem = item;
+            return currentItem.getPrice();
+        }
+        throw new SoldOutException("Sold out, Please buy another item");
     }
 
     @Override
     public void insertCoin(Coin coin) {
-
+        currentBalance = currentBalance + coin.getDenomination();
+        cashInventory.add(coin);
     }
 
     @Override
     public List<Coin> refund() {
-        return null;
+        List<Coin> refund = getChange(currentBalance);
+        updateCashInventory(refund);
+        currentBalance = 0;
+        currentItem = null;
+        return refund;
     }
 
     @Override
     public Bucket<Item, List<Coin>> collectItemAndChange() {
-        return null;
+        Item item = collectItem();
+        totalSales = totalSales + currentItem.getPrice();
+        
+        List<Coin> change = collectChange();
+
+        return new Bucket<Item, List<Coin>>(item, change);
+    }
+
+    private List<Coin> collectChange() {
+        long changeAmount = currentBalance - currentItem.getPrice();
+        List<Coin> change = getChange(changeAmount);
+        updateCashInventory(change);
+        currentBalance = 0;
+        currentItem = null;
+        return change;
+    }
+
+
+    private Item collectItem() throws NotSufficientChangeException, NotFullPaidException {
+        if(isFullPaid()){
+            if(hasSufficientChange()){
+                itemInventory.deduct(currentItem);
+                return currentItem;
+            }
+            throw new NotSufficientChangeException("Not sufficient change in Inventory");
+        }
+        long remainingBalance = currentItem.getPrice() - currentBalance;
+        throw new NotFullPaidException("Price not full paid, remaining : ", remainingBalance);
+    }
+
+    private boolean hasSufficientChange() {
+        return hasSufficientChangeForAmount(currentBalance - currentItem.getPrice());
+
+    }
+
+    private boolean hasSufficientChangeForAmount(long amount) {
+        boolean hasChange = true;
+        try{
+            getChange(amount);
+        } catch (NotSufficientChangeException ex){
+            return hasChange = false;
+        }
+        return hasChange;
+    }
+
+    private List<Coin> getChange(long amount) throws NotSufficientChangeException {
+        List<Coin> changes = Collections.EMPTY_LIST;
+
+        if(amount > 0){
+            changes = new ArrayList<Coin>();
+            long balance = amount;
+            while (balance > 0){
+                if(balance >= Coin.QUARTER.getDenomination() && cashInventory.hasItem(Coin.QUARTER)){
+                    changes.add(Coin.QUARTER);
+                    balance = balance - Coin.QUARTER.getDenomination();
+                    continue;
+                } else if (balance >= Coin.DIME.getDenomination() && cashInventory.hasItem(Coin.DIME)){
+                    changes.add(Coin.DIME);
+                    balance = balance - Coin.DIME.getDenomination();
+                    continue;
+                } else if (balance >= Coin.NICKLE.getDenomination() && cashInventory.hasItem(Coin.NICKLE)){
+                    changes.add(Coin.NICKLE);
+                    balance = balance - Coin.NICKLE.getDenomination();
+                    continue;
+                } else if (balance >= Coin.PENNY.getDenomination() && cashInventory.hasItem(Coin.PENNY)){
+                    changes.add(Coin.PENNY);
+                    balance = balance - Coin.PENNY.getDenomination();
+                    continue;
+                } else {
+                    throw new NotSufficientChangeException("Not Sufficient Change, Please try another product");
+                }
+
+            }
+        }
+        return changes;
+    }
+
+    private boolean isFullPaid() {
+        if(currentBalance >= currentItem.getPrice()){
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public void reset() {
-
+    public void reset(){
+        cashInventory.clear();
+        itemInventory.clear();
+        totalSales = 0;
+        currentItem = null;
+        currentBalance = 0;
     }
+
+    public void printStats(){
+        System.out.println("Total Sales : " + totalSales);
+        System.out.println("Current Item Inventory : " + itemInventory);
+        System.out.println("Current Cash Inventory : " + cashInventory);
+    }
+
+    private void updateCashInventory(List<Coin> change) {
+        for(Coin c : change){
+            cashInventory.deduct(c);
+        }
+    }
+
+    public long getTotalSales(){
+        return totalSales;
+    }
+
+
 }
